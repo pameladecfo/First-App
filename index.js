a60d03103fc375e0d8d54efe29e8aacb0cc62727
pameladecfo <pameladecfo@gmail.com>
2021-08-09 09:20:28 -0300

    Modificações
    Modificações feitas tendo em vista a aula de sexta feira

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
-const { route } = require("./routes");
 const app = express();
-const routes = require("./routes")
+const routes = require("./routes");
+const expressLayouts = require("express-ejs-layouts");
 
 const port=3000;
 const address = "localhost";
 
+//Ativa uso do EJS e do Express-ejs-layouts
+app.set('view engine','ejs');
+app.use(expressLayouts);
+
+//Criando e usando rotas simples com o Node.js e o Express
 app.use('/',routes);
 
 //Criando um servidor simples com o Node.js e o Express
+
 const server = app.listen(port,address,()=>{
- let host = server.address().address;
- let port = server.address().port;
+     let host = server.address().address;
+     let port = server.address().port;
  console.log(`Servidor executando no endereço ${host} e porta ${port}`);
 });
--- routes.js
+++ routes.js
@@ -1,14 +1,58 @@
 const express = require("express");
 const router = express.Router();
+const app = express();
 //const faker = require("faker");
 
-let db = require("./db");
+//let db = require("./db");
+
+//db.criarDB("minhaBaseDados");
+
+//Especifica a pasta contendo arquivos estáticos. 
+//O nome 'public' não precisará ser colocado na rota 
+//Para serem alcançados os arquivos e pastas que estão dentro dele. 
+//Por isso na imagem que está na página home.ejs só há o indicativo para 'images'
+router.use(express.static('public'));
+
 
 router.get('/',(req,res)=>{
-    res.send('Minha Página Home!!');
+  res.render('pages/home');
 });
-  router.get('/about', ()=>{
-      res.send('Minha página Sobre');
-  });
+router.get('/about', (req,res)=>{
+  res.render('pages/about');
+});
+
+router.post('/cadastro/remove',(req,res)=>{
+  let usuario={name: "pamela", email:"pameladecfo@gmail.com"};
   
+  /* for(let cont=1;cont<=6;cont++){
+        usuarios.push({name:faker.name.findName(),email: faker.internet.email(),avatar: faker.image.image()});
+    } */
+
+    let result = db.inserirDado(usuario);
+    console.log(result);
+    //res.render('pages/insert',{result});
+});
+
+router.get('/cadastro',(req,res)=>{
+  
+  res.render('pages/cadastro');
+});
+
+router.get('/cadastro/insert',(req,res)=>{
+  let usuario={name: "wellington", email: "wwagner@virtual.ufc.br"};
+  /* for(let cont=1;cont<=6;cont++){
+      usuarios.push({name:faker.name.findName(),email: faker.internet.email(),avatar: faker.image.image()});
+  } */
+
+  let result = db.inserirDado(usuario);
+  console.log(result);
+  //res.render('pages/insert',{result});
+});
+
+router.get('/cadastro/list',(req,res)=>{
+
+});
+
+//Essa linha permite que este código seja exportado como um módulo e possa ser usado em outras partes da aplicação.
 module.exports = router;
+
