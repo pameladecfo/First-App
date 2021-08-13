447e6026a657077536a1bc1f5296b861869f545e
pameladecfo <pameladecfo@gmail.com>
2021-08-13 11:54:41 -0300

    Modificações 13/08

--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const app = express();
-//const faker = require("faker");
 
-//let db = require("./db");
+// const faker = require("faker");
 
-//db.criarDB("minhaBaseDados");
+// let db = require("./db");
+
+// db.criarDB("minhaBaseDados");
 
 //Especifica a pasta contendo arquivos estáticos. 
 //O nome 'public' não precisará ser colocado na rota 
@@ -14,39 +14,61 @@ const app = express();
 router.use(express.static('public'));
 
 
-router.get('/',(req,res)=>{
-  res.render('pages/home');
+
+//************* Exemplode Rotas ************* 
+
+/*
+http://localhost:3030/css
+http://localhost:3030/images
+http://localhost:3030/index.html
+/ = http://localhost:3030/
+/about = http://localhost:3030/about
+/cadastro =  http://localhost:3030/cadastro
+*/
+
+
+//Req é um objeto que recebe dados da requisição HTTP feita (request). Res permite enviar uma resposta ao navegador (Response)
+router.get('/',(req,res)=>{ //callback - funcao que trata dado evento GET
+    res.render('pages/home');
 });
-router.get('/about', (req,res)=>{
-  res.render('pages/about');
+
+router.get('/about',(req,res)=>{ //callback - funcao que trata dado evento  GET
+
+    res.render('pages/about');
+});
+
+router.get('/cadastro',(req,res)=>{ //callback - funcao que trata dado evento  GET
+
+    //a funcao render pode receber um pametro na forma de objeto literal
+    //no caso, ela irá receber um objeto com campo chamado users e com valor igual ao vetor users
+    res.render('pages/cadastro',{users:users}); 
 });
 
 router.post('/cadastro/remove',(req,res)=>{
-  let usuario={name: "pamela", email:"pameladecfo@gmail.com"};
-  
-  /* for(let cont=1;cont<=6;cont++){
-        usuarios.push({name:faker.name.findName(),email: faker.internet.email(),avatar: faker.image.image()});
-    } */
+    let item =req.body.id; //pega o valor passado através do parâmetro id e atribui a variável item. 
 
-    let result = db.inserirDado(usuario);
-    console.log(result);
-    //res.render('pages/insert',{result});
+    users.splice(item,1); //este método permite adicionar ou remover um item do vetor em uma dada posição. 
+    //res.render('pages/cadastro',{users:users});
+    console.log("Elementos cadastrados: ",users);
+    res.sendStatus(200); //envia mensagem 200 significando que as modificacoes foram ok
 });
 
-router.get('/cadastro',(req,res)=>{
-  
-  res.render('pages/cadastro');
-});
 
-router.get('/cadastro/insert',(req,res)=>{
-  let usuario={name: "wellington", email: "wwagner@virtual.ufc.br"};
-  /* for(let cont=1;cont<=6;cont++){
-      usuarios.push({name:faker.name.findName(),email: faker.internet.email(),avatar: faker.image.image()});
-  } */
 
-  let result = db.inserirDado(usuario);
-  console.log(result);
-  //res.render('pages/insert',{result});
+router.post('/cadastro/update',(req,res)=>{
+    //substitui os valores armazenados no item do vetror dado por id, por valores fornecidos como parametro vindos do navegador.
+    //recebe dados do cliente na forma de um objeto JSON
+
+    users[req.body.id].name=req.body.name;
+    users[req.body.id].email=req.body.email;
+    users[req.body.id].address=req.body.address;
+    users[req.body.id].age=req.body.age;
+    users[req.body.id].height=req.body.height;
+    users[req.body.id].vote=req.body.vote;
+
+    console.log("Dados recebidos: ",req.body);//mostra no console do servidor os dados recebidos
+
+    res.sendStatus(200); //envia mensagem 200 significando que as modificacoes foram ok
 });
 
 router.get('/cadastro/list',(req,res)=>{
